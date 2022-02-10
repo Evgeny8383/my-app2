06e6b7be59e8fa53e0b551573292e030258ca518
evgeny8383 <baraban8383@gmail.com>
2022-02-11 00:21:24 +0700

    add photo

--- src/others.js
+++ src/others.js
@@ -1,11 +1,13 @@
-import React from "react"
+import React from "react";
+import Header from "./header"
 
 
 
 export default function Others(){
     return(
         <div>
-            <h1>Others</h1>
+           <Header/>
+           <h2 className="up_header">Достопримечательности Барнаула</h2>
         </div>
     )
 
